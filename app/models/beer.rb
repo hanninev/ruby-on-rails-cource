35509d131e9cdea42a4384ba805da2c96c3f31f5
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-09-14 19:43:25 +0300

    viikko 2, tehtävät neloseen asti

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,9 @@
 class Beer < ApplicationRecord
 	belongs_to :brewery
 	has_many :ratings
+
+	def average_rating
+		"Has #{self.ratings.length} ratings, avegage #{self.ratings.sum { |r| r.score }/self.ratings.length}"
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :breweries
   root 'breweries#index'
   get 'ratings', to: 'ratings#index'
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
