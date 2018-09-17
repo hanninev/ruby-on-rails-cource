0a1aae1a89845c1913b984ae916dae8de6327e79
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-09-17 23:27:14 +0300

    viikko 3, tehtävä 11

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -5,19 +5,23 @@ class MembershipsController < ApplicationController
   
     def new
       @membership = Membership.new
-      @beer_clubs = BeerClub.all
+      @beer_clubs = BeerClub.all.select{|bc| !current_user.beer_clubs.include? bc}
     end
   
     def create
       @membership = Membership.new params.require(:membership).permit(:beer_club_id)
       @membership.user = current_user
-    
+      
+      if !current_user.beer_clubs.include? @membership.beer_club
       if @membership.save
         redirect_to user_path current_user
-      else
-        @beer_clubs = BeerClub.all
+      else  
+        @beer_clubs = BeerClub.all.select{|bc| !current_user.beer_clubs.include? bc}
         render :new
       end
+    end
+      
+      
     end
   
     def destroy
