1d092919ac2ecd8ef79708b1371dabc1caa7962e
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-09-18 01:37:16 +0300

    viikko 3, tehtävä 8

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -3,7 +3,11 @@ class Brewery < ApplicationRecord
   has_many :ratings, through: :beers
 
   validates :name, length: { minimum: 1 } 
-  validates :year, numericality: { greater_than_or_equal_to: 1040, less_than_or_equal_to: 2018}
+  validates :year, numericality: { greater_than_or_equal_to: 1040, less_than_or_equal_to: :this_year}
 
   include RatingAverage
+
+  def this_year
+    Time.now.year
+  end
 end
