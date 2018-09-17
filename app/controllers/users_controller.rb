07eeeb9ab729b198b654059589323c1dd536f30e
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-09-18 01:26:35 +0300

    viikko 3, tehtävä 16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,14 +42,12 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       user = User.find params[:id]
-      if current_user == user
-      if user.update(user_params) 
+      if user_params[:username].nil? and @user == current_user and @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
       end
     end
   end
