4e6c38906aaad97d562a2b4e6795c7f6c635976a
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-09-18 00:39:40 +0300

    viikko 3, tehtävä 13

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,11 @@ require 'concerns/rating_average'
 class User < ApplicationRecord
   include RatingAverage
 
+  has_secure_password                  
+
   validates :username, uniqueness: true,
                        length: { minimum: 3, maxinum: 50 }
-
+  validates :password, length: { minimum: 4 }, format: { with: /.*[A-Z].*\d/, message: 'must contain an uppercase letter and a digit' }
   has_many :ratings
   has_many :membership
   has_many :beers, through: :ratings
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_17_100059) do
+ActiveRecord::Schema.define(version: 2018_09_17_202801) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2018_09_17_100059) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
