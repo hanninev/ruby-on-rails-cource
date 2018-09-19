2c15c6013ec81d39318c9d4888d52b2291347664
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-09-19 17:31:18 +0300

    viikko 4, tehtävä 2

---
+++ spec/models/beer_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Beer, type: :model do
+  it "is saved if a name, brewery and style are given" do
+    test_brewery = Brewery.new name: "test", year: 2000
+    beer = Beer.create name:"Koff", brewery: test_brewery, style:"Lager"
+
+    expect(beer).to be_valid
+    expect(Beer.count).to eq(1)
+  end
+
+  it "is not saved if a name isn't given" do
+    test_brewery = Brewery.new name: "test", year: 2000
+    beer = Beer.create brewery: test_brewery, style:"Lager"
+
+    expect(beer).not_to be_valid
+    expect(Beer.count).to eq(0)
+  end
+
+  it "is not saved if a style isn't given" do
+    test_brewery = Brewery.new name: "test", year: 2000
+    beer = Beer.create name:"Koff", brewery: test_brewery
+
+    expect(beer).not_to be_valid
+    expect(Beer.count).to eq(0)
+  end
+end
