0b9e4924393465c873543e82c49f667d9dca8366
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-10-05 19:34:39 +0300

    viikko 5, tehtävä 4

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,12 +1,17 @@
 class BeermappingApi
     def self.places_in(city)
+      city = city.downcase
+      Rails.cache.fetch(city, expires_in: 1.week) { get_places_in(city) }
+    end
+    
+    def self.get_places_in(city)
       url = "http://beermapping.com/webservice/loccity/#{key}/"
-  
+    
       response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
       places = response.parsed_response["bmp_locations"]["location"]
-  
+    
       return [] if places.is_a?(Hash) and places['id'].nil?
-  
+    
       places = [places] if places.is_a?(Hash)
       places.map do | place |
         Place.new(place)
@@ -16,4 +21,4 @@ class BeermappingApi
     def self.key
       "8d58f9e92fe8b17867ac736fba89c48b"
     end
-  end
+end
