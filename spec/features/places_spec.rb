5868807f4aeb76eb3ae228879146ba8983213ca3
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-10-05 17:43:01 +0300

    viikko 5, tehtävä 2

---
+++ spec/features/places_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+describe "Places" do
+  it "if one is returned by the API, it is shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [ Place.new( name:"Oljenkorsi", id: 1 ) ] )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+  end
+
+  it "if more than one is returned by the API, they are shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [ Place.new( name:"Oljenkorsi", id: 1 ),
+    Place.new( name:"Kuudes linja", id: 2) ]
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "Oljenkorsi"
+    expect(page).to have_content "Kuudes linja"
+  end
+
+  it "If none is returned by the API, the page shows 'No locations'-message" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      []
+    )
+
+    visit places_path
+    fill_in('city', with: 'kumpula')
+    click_button "Search"
+
+    expect(page).to have_content "No locations in kumpula"
+  end
+end
