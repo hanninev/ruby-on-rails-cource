60c09126730470690509aba8d49be2a148aadd52
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-10-07 21:56:51 +0300

    viikko 5, tehtävät 7 ja 8

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -12,12 +12,15 @@ class MembershipsController < ApplicationController
     @membership = Membership.new params.require(:membership).permit(:beer_club_id)
     @membership.user = current_user
 
-    if !current_user.beer_clubs.include?(@membership.beer_club) && @membership.save
-      redirect_to user_path current_user
-    else
-      @beer_clubs = BeerClub.all.reject{ |bc| current_user.beer_clubs.include? bc }
-      render :new
-    end
+    respond_to do |format|
+      if !current_user.beer_clubs.include?(@membership.beer_club) && @membership.save
+          format.html { redirect_to beer_club_path(@membership.beer_club), notice: "#{current_user.username} welcome to beer club!" }
+          format.json { render :show, status: :created, location: @beer_club }
+        else
+          @beer_clubs = BeerClub.all.reject{ |bc| current_user.beer_clubs.include? bc }
+          format.html { render :new }
+        end
+      end
   end
 
   def destroy
