2b002c57af3f943366c92a5d0d3286de3c30581c
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-10-11 17:57:43 +0300

    korjaus

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,10 @@
 module ApplicationHelper
   def edit_and_destroy_buttons(item)
-    unless if current_user.nil?
-      edit = link_to('Edit', url_for([:edit, item]), class: "btn btn-primary")
-      del = link_to('Destroy', item, method: :delete,
-                                     data: { confirm: 'Are you sure?' },
-                                     class: "btn btn-danger")
-      raw("#{edit} #{del}")
-    end
+    edit = link_to('Edit', url_for([:edit, item]), class: "btn btn-primary")
+    del = link_to('Destroy', item, method: :delete,
+                                   data: { confirm: 'Are you sure?' },
+                                   class: "btn btn-danger")
+    raw("#{edit} #{del}") unless current_user.nil?
   end
 
   def round(number)
