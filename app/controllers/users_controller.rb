6a98c6bd6e65af5b78d36b65430f0db771f6422a
Ville Hänninen <ville.hanninen@helsinki.fi>
2018-10-11 21:39:59 +0300

    lisäys user-sivulle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
+    @confirmed_memberships = @user.membership.reject{ |m| m.confirmed.nil? }.map{ |m| m.beer_club }
+    @applications = @user.membership.reject{ |m| m.confirmed? }.map{ |m| m.beer_club }
   end
 
   # GET /users/new
